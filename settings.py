bc07ffe1e84c2494024654b64ba8eeb33ac1522f
richardsonclay <c@oneschool.com>
2011-10-21 11:30:13 -0700

    added sqlite3 database settings

--- settings.py
+++ settings.py
@@ -1,4 +1,8 @@
 # Django settings for mythoughtjot project.
+import os
+
+APP_PATH = os.path.dirname(__file__)
+DATABASE_PATH = os.path.join(APP_PATH, 'database.db')
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -11,12 +15,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': DATABASE_PATH,                  # Or path to database file if using sqlite3.
+        'USER': '',                             # Not used with sqlite3.
+        'PASSWORD': '',                         # Not used with sqlite3.
+        'HOST': '',                             # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                             # Set to empty string for default. Not used with sqlite3.
     }
 }
 
