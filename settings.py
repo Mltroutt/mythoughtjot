5b01a117366b7e3e903b4f2111bd893cc176ed6f
richardsonclay <c.richardson917@gmail.com>
2011-10-24 17:04:51 -0700

    fixed settings for local development

--- settings.py
+++ settings.py
@@ -1,7 +1,11 @@
 # Django settings for mythoughtjot project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
+
+APP_PATH = os.path.dirname(__file__)
+DATABASE_PATH = os.path.join(APP_PATH, 'database.db')
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
@@ -11,10 +15,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'mythoughtjot_dev_db',                      # Or path to database file if using sqlite3.
-        'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': 'root',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': DATABASE_PATH,                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
