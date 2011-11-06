f0c8cf462de7457e9cdd91b7c81c8fc33ca4df31
Michael <mltroutt@gmail.com>
2011-11-06 12:20:45 -0600

    Added simple canvas view and template

--- canvas/views.py
+++ canvas/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import get_object_or_404, render_to_response
+from django.core.context_processors import csrf
+from mythoughtjot.settings import MEDIA_ROOT, MEDIA_URL
+from mythoughtjot.canvas.models import *
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def canvas(request, pk):
-    return render_to_response("canvas.html", {'pk':pk})
+    #user = UserProfile.object.get(user=pk)
+    canvas = Canvas.objects.get(pk=pk)
+    nodes = Node.objects.filter(canvas=pk)
+    return render_to_response("canvas.html", add_csrf(request, pk=pk, canvas=canvas, nodes=nodes, media_url=MEDIA_URL))
 
 def node(request, pk):
     return render_to_response("node.html", {'pk':pk})
+
+
+def add_csrf(request, **kwargs):
+    """Add CSRF to dictionary."""
+    d = dict(user=request.user, **kwargs)
+    d.update(csrf(request))
+    return d
