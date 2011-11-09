6bd3281f9e54fa0131fd7d402fbce9887ec75755
Michael <mltroutt@gmail.com>
2011-11-09 14:18:47 -0600

    Added/fixed media urls

--- urls.py
+++ urls.py
@@ -4,6 +4,7 @@ from django.views.generic.simple import direct_to_template
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+from django.conf import settings
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -14,7 +15,8 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    url(r'^$', direct_to_template, {'template': 'index.html'}),
+    url(r'^$', direct_to_template, {'template': 'djangoish_templates/index.html'}),
+    url(r'^login/$', direct_to_template, {'template': 'djangoish_templates/login.html'}),
     #url(r'^profiles/', include('profiles.urls')),
     url(r'^canvas/(\d+)/$', 'canvas.views.canvas'),
     url(r'^node/(\d+)/$', 'canvas.views.node'),
@@ -23,3 +25,14 @@ urlpatterns = patterns('',
     url(r'^', include('registration.backends.default.urls')),
 
 )
+
+if settings.DEBUG:
+    from django.views.static import serve
+    _media_url = settings.MEDIA_URL
+    if _media_url.startswith('/'):
+        _media_url = _media_url[1:]
+        urlpatterns += patterns('',
+            (r'^%s(?P<path>.*)$' % _media_url,
+             serve,
+            {'document_root': settings.MEDIA_ROOT}))
+    del(_media_url, serve)
