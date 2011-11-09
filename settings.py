d9565a6e5bbed501285ab8793ca64bc329f1db2b
Michael <mltroutt@gmail.com>
2011-11-09 14:26:41 -0600

    Fixed static settings

--- settings.py
+++ settings.py
@@ -64,7 +64,7 @@ STATIC_ROOT = os.path.join(APP_PATH, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = ''
+STATIC_URL = '/static/'
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
