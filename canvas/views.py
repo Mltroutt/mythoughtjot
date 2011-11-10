2ec2528a2751ebcdc1a161ebf4269e7b1bf52b77
Michael Troutt <mltroutt@gmail.com>
2011-11-10 11:27:12 -0600

    User must now be a collaborator to view a canvas

--- canvas/views.py
+++ canvas/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, render_to_response
+from django.shortcuts import get_object_or_404, render_to_response, redirect
 from django.core.context_processors import csrf
 from mythoughtjot.settings import MEDIA_ROOT, MEDIA_URL
 from mythoughtjot.canvas.models import *
@@ -7,6 +7,9 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def canvas(request, pk):
     canvas = Canvas.objects.get(pk=pk)
+    isCollaborator = Canvas.objects.filter(pk=pk,collaborators__username=request.user.username)
+    if not isCollaborator:
+        return redirect('/')
     nodes = Node.objects.filter(canvas=pk)
     return render_to_response("canvas.html", add_csrf(request, pk=pk, canvas=canvas, nodes=nodes, media_url=MEDIA_URL))
 
