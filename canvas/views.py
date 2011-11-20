c8efafceac6a81a62d08af88755f6f1152616255
Michael <mltroutt@gmail.com>
2011-11-20 10:10:21 -0600

    Added 'public' status to canvas

--- canvas/views.py
+++ canvas/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import get_object_or_404, render_to_response, redirect
 from django.core.context_processors import csrf
-from mythoughtjot.settings import MEDIA_ROOT, MEDIA_URL
 from mythoughtjot.canvas.models import *
 from django.contrib.auth.decorators import login_required
+from django.template import RequestContext
 
 @login_required
 def canvas(request, pk):
     canvas = Canvas.objects.get(pk=pk)
     isCollaborator = Canvas.objects.filter(pk=pk,collaborators__username=request.user.username)
-    if not isCollaborator:
+    if not isCollaborator and not canvas.public:
         return redirect('/')
     nodes = Node.objects.filter(canvas=pk)
-    return render_to_response("canvas.html", add_csrf(request, pk=pk, canvas=canvas, nodes=nodes, media_url=MEDIA_URL))
+    return render_to_response("canvas/canvas.html", add_csrf(request, pk=pk, canvas=canvas, nodes=nodes), context_instance=RequestContext(request))
 
 def node(request, pk):
     return render_to_response("node.html", {'pk':pk})
