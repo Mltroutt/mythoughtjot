dd61f55382ad8cfbe4e4e2b9dd0d8a27156e4fd9
Michael Troutt <mltroutt@gmail.com>
2011-11-21 12:37:28 -0600

    Finished validtion for project/canvas creation

--- canvas/views.py
+++ canvas/views.py
@@ -3,54 +3,67 @@ from django.core.context_processors import csrf
 from mythoughtjot.canvas.models import *
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
-from django.forms import ModelForm
+from django.forms import ModelForm, forms
 
 
 class ProjectForm(ModelForm):
     class Meta:
         model = Project
         exclude = ["created","owner","creator","project_collaborators"]
+        
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop('request', None)
+        super(ProjectForm, self).__init__(*args, **kwargs)
+
 
 class CanvasForm(ModelForm):
     class Meta:
         model = Canvas
         fields = ('project','title','public','allow_guests')
+        
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop('request', None)
+        super(CanvasForm, self).__init__(*args, **kwargs)
+
+    def clean_project(self):
+        project = self.cleaned_data['project']
+        if not Project.objects.filter(pk=project.pk,project_collaborators=self.request.user).exists():
+            #seld.form.data['project'] = ''
+            raise forms.ValidationError("You are not a collaborator on this project.")
+        return project
+
+# TODO: add ability to add collaborators to projects/canvas, add user submission validation to canvas
 
 @login_required
-def create_project(request):
-    form = ProjectForm(request.POST or None)
+def create_project(request=None):
+    form = ProjectForm(request.POST or None, request=request)
 
-    if request.method == 'POST':
-        if form.is_valid():
-            pre_save = form.save(commit=False)
-            pre_save.owner = request.user
-            pre_save.creator = request.user
-            pre_save.save()
-            pre_save.project_collaborators.add(request.user)
-            return redirect('/project/')
+    if request.method == 'POST' and form.is_valid():
+        pre_save = form.save(commit=False)
+        pre_save.owner = request.user
+        pre_save.creator = request.user
+        pre_save.save()
+        pre_save.project_collaborators.add(request.user)
+        return redirect('/project/')
     return render_to_response('project_creation.html', add_csrf(request, form=form), context_instance=RequestContext(request))
 
 @login_required
 def create_canvas(request):
-
-    if request.method == 'POST':
-        form = CanvasForm(request.POST)
-        form.fields['project'].queryset = Project.objects.filter(owner=request.user)
-        if form.is_valid():
-            pre_save = form.save(commit=False)
-            pre_save.owner = request.user
-            pre_save.creator = request.user
-            pre_save.save()
-            pre_save.collaborators.add(request.user)
-            return redirect('/canvas/')
-    form = CanvasForm()
+    form = CanvasForm(request.POST or None, request=request)
+    if request.method == 'POST' and form.is_valid():
+        pre_save = form.save(commit=False)
+        pre_save.owner = request.user
+        pre_save.creator = request.user
+        pre_save.save()
+        pre_save.collaborators.add(request.user)
+        return redirect('/canvas/')
     form.fields['project'].queryset = Project.objects.filter(project_collaborators=request.user)
     return render_to_response('canvas_creation.html', add_csrf(request, form=form), context_instance=RequestContext(request))
 
 @login_required
 def canvas(request, pk):
     canvas = Canvas.objects.get(pk=pk)
-    isCollaborator = Canvas.objects.filter(pk=pk,collaborators__username=request.user.username)
+    isCollaborator = Canvas.objects.filter(pk=pk,collaborators=request.user).exists()
     if not isCollaborator and not canvas.public:
         return redirect('/')
     nodes = Node.objects.filter(canvas=pk)
