5b186df6e935ff0f1eb648f108114a030651aeef
Michael Troutt <mltroutt@gmail.com>
2011-12-06 15:20:59 -0600

    Added and fixed a lot of javascript

--- media/js/forms/canvas.js
+++ media/js/forms/canvas.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
 	$("form#simple_form").submit(function(e) {
+		submit_button = $('input[type=submit]', this);
+		submit_button.attr('disabled', 'disabled');
 		project = $("#id_project").val();
 		title = $("#id_title").val();
-		public_option = $("#id_public").val();
-		guest_option = $("#id_allow_guests").val();
+		public_option = $("#id_public").is(':checked');
+		guest_option = $("#id_allow_guests").is(':checked');
 		csrf = $("input[name=csrfmiddlewaretoken]").val();
 		$.post('', {'project' : project, 'title' : title, 'public' : public_option, 'allow_guests' : guest_option, 'csrfmiddlewaretoken': csrf}, function(data) {
 			$('ul.errorlist').remove();
@@ -15,16 +17,19 @@ $(document).ready(function() {
 		  	}else{
 	  			for (error in data['errors'])
 				$("#id_" + error).after(data['errors'][error]);
+				submit_button.removeAttr("disabled");
 		  	}
-			console.log(data);
+			//console.log(data);
 		});
 		return false;
 	});
 	$("form#ajax_form").submit(function(e) {
+		submit_button = $('input[type=submit]', this);
+		submit_button.attr('disabled', 'disabled');
 		project = $("#id_project").val();
 		title = $("#id_title").val();
-		public_option = $("#id_public").val();
-		guest_option = $("#id_allow_guests").val();
+		public_option = $("#id_public").is(':checked');
+		guest_option = $("#id_allow_guests").is(':checked');
 		csrf = $("input[name=csrfmiddlewaretoken]").val();
 		$.post('/canvas/create_modal/', {'project' : project, 'title' : title, 'public' : public_option, 'allow_guests' : guest_option, 'csrfmiddlewaretoken': csrf}, function(data) {
 			$('ul.errorlist').remove();
@@ -37,8 +42,9 @@ $(document).ready(function() {
 		  	}else{
 	  			for (error in data['errors'])
 				$("#id_" + error).after(data['errors'][error]);
+				submit_button.removeAttr("disabled");
 		  	}
-			console.log(data);
+			//console.log(data);
 		});
 		return false;
 	});
